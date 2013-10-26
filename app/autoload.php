40ef7177fbac9ca2d60b64655a06e55c7f68bf09
Irvyne <thibaud.bardin@gmail.com>
2013-10-26 20:05:08 +0200

    Add autoloader file

---
+++ app/autoload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Thibaud BARDIN (Irvyne)
+ * This code is under the MIT License (https://github.com/Irvyne/license/blob/master/MIT.md)
+ */
+
+use Composer\Autoload\ClassLoader;
+
+/**
+ * @var ClassLoader $loader
+ */
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+return $loader;
